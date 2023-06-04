46877350bb6efb5926d8445bb54e4e30be8950d6
tpn006 <tpn006@ucsd.edu>
2023-06-03 17:48:04 -0700

    hoorah

--- mrsoftware/KnownMotif.py
+++ mrsoftware/KnownMotif.py
@@ -20,52 +20,9 @@ class KnownMotif:
         self.pwm = pwm
         self.score_arr = []
         self.rnd_scr_arr = []
-    
-    def calcAndSetThreshold(self, background_sequences, pval):
-        """
-        Given a p-value threshold determine what score that is and set the threshold variable to that score
-        """
-        self.pvalThresh = pval
-        for seq in background_sequences:
-            self.scoreRandSequence(str(seq))
-        #print(self.name + ' length of rnd array: ' + str(len(self.rnd_scr_arr)))
-        #print('length of score array: ' + str(len(self.score_arr)))
-        num = int(len(self.rnd_scr_arr) * (1-pval))
-        self.rnd_scr_arr.sort()
-        self.threshold_score = self.rnd_scr_arr[num]
-        # print(self.rnd_scr_arr)
-        self.setPval()
 
-        '''i = 1
-        temp_dist = self.rnd_scr_arr.copy()
-        while i < num:
-            temp_dist.pop(temp_dist.index(max(temp_dist)))
-            i+=1
-        thresh = max(temp_dist)
-        self.threshold = thresh'''
-        #self.graphThreshold()
-        #print(str("SCORE ARRAY") + str(self.rnd_scr_arr))
-
-    
-    """    def setPval(self):
-        peak_total = 0
-        peak_motif = 0
-        bg_total = 0
-
-        bg_motif  = 0 #FIX THIS LATER
-        # TODO figure out why the self.rnd_scr_arr is so long
-        peak_total = len(self.score_arr)
-        for score in self.score_arr:
-            if score >= self.threshold:
-                peak_motif += 1
-        for score in self.rnd_scr_arr:
-            if score >= self.threshold:
-                bg_motif += 1
-        #print("Running scipy with threshold "+ str(self.threshold) +" Peak total: " + str(peak_total) + " peak motif: " + str(peak_motif) + " Background total: " + str(peak_total) + " bgrnd motif: " + str(bg_motif))
-        self.pval = KnownMotif.ComputeEnrichment(peak_total, peak_motif, peak_total, bg_motif)
-    """
     def quickScore(self, sequence):
-        if sequence == None: # IF we get grabage input :(
+        if sequence == None: # IF we get bad input :(
             return
         scores = []
         # Make a shifting window to go through the pwm with
@@ -87,7 +44,24 @@ class KnownMotif:
         return max(scores)
 
     
-    def magicDoAllFunction(self, peak_seqs, back_seqs, p_val = 0.001): # For most promoter datasets, motifs with a p-value of more than 1e-10 or even 1e-12 are likely to be false positives. (from HOMER)
+    def scoreAllGetThresholdScoreCalcPValue(self, peak_seqs, back_seqs, p_val = 0.01): 
+        """
+        Scores all sequences (peak seqs and random background seqs, determines threshold score, and calculates p value for motif)
+        
+        Parameters:
+        -----------
+        peak_seqs: array of strs
+            list of all peak sequences from genome
+        back_seqs: array of strs
+            list of all randomly generated sequences
+        p_val: float
+            number used to calculate threshold score
+
+        Returns:
+        --------
+        Nothing
+        """
+        
         # Get the threshold score by scoring all background sequences
         print("Evaluating " + str(self.name))
         back_scores = []
@@ -122,98 +96,29 @@ class KnownMotif:
                 sig_seq += 1
 
         # p value the stuff
-        # table = [[num peaks that pass threshold, num peaks that don't pass],[num background that pass, num background that doesnt pass]]
-        table = [[sig_seq, len(peak_scores)-sig_seq], [num_expected_to_pass_at_pval, len(back_scores)-num_expected_to_pass_at_pval]]
+        # table = [[num peaks that pass threshold, num peaks that don't pass],
+        #          [num background that pass, num background that doesnt pass]]
+        table = [[sig_seq, len(peak_scores)-sig_seq],
+                 [num_expected_to_pass_at_pval, len(back_scores)-num_expected_to_pass_at_pval]]
         # print("Our fisher exact table for " + self.name+ " shows: " + str(table))
         odds, pval = scipy.stats.fisher_exact(table)
-        self.pval = pval
+        self.pval = pval 
         print("Done evaluating " + self.name + " resulting pval is " + str(self.pval))
-        
-
-
-        
-
-
-    def setPval(self):
-        #Assume threshold already set
-        fwd_peak_pass = 0
-        for seq in self.score_arr:
-            if seq > self.threshold_score:
-                fwd_peak_pass += 1  
-        peak_total = len(self.score_arr)
-        bg_total = peak_total # FOR NOW BECAUSE rnd_scr_arr is growing for no reason
-        bg_motif= len(self.rnd_scr_arr) * self.pvalThresh
-        table = [[fwd_peak_pass, peak_total-fwd_peak_pass], [bg_motif, bg_total-bg_motif]]
-        odds, pval = scipy.stats.fisher_exact(table)
-        self.pval = pval
-        #self.pval = self.ComputeEnrichment(len(self.score_arr), fwd_peak_pass, len(self.score_arr), num_bg_pass)
-
-    """
-    def updateScore(self, val, which_score):
-        
-        #updates score by appending the value
-        
-        #print('before update score: ' + str(len(self.rnd_scr_arr)))
-        if "forward" in which_score:
-            self.score_arr.append(float(val))
-        elif "random" in which_score:
-            self.rnd_scr_arr.append(float(val))
-        #print('after update score ' + str(len(self.rnd_scr_arr)))
-    """
-
-    def updateScore(self, val):
-        self.score_arr.append(float(val))
-
-    def updateScoreRand(self, val):
-        self.rnd_scr_arr.append(float(val))
-
-    def scoreSequence(self, sequence):
-        """
-        Given a long sequence from a peak, find where this motif is best suited and then update the score.
-        Calls a lot of scoreSeq with shorter sequences
-        """
-        # list of scores. scores[i] should give the score of the substring sequence[i:i+n]. The default value for the score will be the maximum negative value so that the highest will always overwrite
-        #scores = [0]*(len(sequence)-self.wscoreRandSequence + 1)
-        scores = []
-        i = 0
-        # score the PWM on all spots in the sequence
-        while i < ( len(sequence) - self.w + 1 ):
-            scores.append(self.maxScore(sequence[i : i + self.w]))
-            i += 1
-        scores.sort()
-        if len(scores) != 0:
-            self.updateScore(scores[len(scores)-1]) #update score with the highest score found (last thing when sorted is the highest score)
-        
-    def scoreRandSequence(self, sequence):
-        """
-        Given a long sequence from a peak, find where this motif is best suited and then update the score.
-        Calls a lot of scoreSeq with shorter sequences
-        """
-        # list of scores. scores[i] should give the score of the substring sequence[i:i+n]. The default value for the score will be the maximum negative value so that the highest will always overwrite
-        #scores = [0]*(len(sequence)-self.w + 1)
-        scores = []
-        i = 0
-        # score the PWM on all spots in the sequence
-        while i < ( len(sequence) - self.w + 1 ):
-            scores.append(self.maxScore(sequence[i : i + self.w -1]))
-            i += 1
-        scores.sort()
-        if len(scores) != 0:
-            self.updateScoreRand(max(scores)) #update score with the highest score found (last thing when sorted is the highest score)
-    # WHY SITN THSI WORKING
 
     def ReverseComplement(self, sequence):
-        """ Get the reverse complement of a sequence
-                Parameters
-                ----------
-                sequence : str
-                Sequence of nucleotides
-                
-                Returns
-                -------
-                revcomp : str
-                Reverse complement of sequence
         """
+        Get the reverse complement of a sequence
+                
+        Parameters
+        ----------
+        sequence : str
+            Sequence of nucleotides
+            
+        Returns
+        -------
+        revcomp : str
+            Reverse complement of sequence
+            """
         revcomp = ""
         sequence = str.upper(sequence)
         i = len(sequence)-1
@@ -226,50 +131,6 @@ class KnownMotif:
                 
         #print(str(len(sequence)) + " compared to " + str(len(revcomp)))
         return revcomp
-        """
-        while i >= 0:
-            if sequence[i] == 'A':
-                revcomp = revcomp + 'T'
-            elif sequence[i] == 'C':
-                revcomp = revcomp + 'G'
-            elif sequence[i] == 'G':
-                revcomp = revcomp + 'C'
-            elif sequence[i] == 'T':
-                revcomp = revcomp + 'A'
-            elif sequence[i] == 'N':
-                revcomp = revcomp + 'N'
-            i = i-1
-        return revcomp
-        """
-
-    def maxScore(self, sequence):
-        """
-        Score the sequence and its reverse complement and determine which is better
-        """
-        fwd_score = self.scoreSeq(sequence)
-        bwd_score = self.scoreSeq(self.ReverseComplement(sequence))
-        #print("Forward: " + str(fwd_score) + " Backward: " + str(bwd_score))
-        if fwd_score >= bwd_score:
-            return fwd_score
-        else:
-            return bwd_score
-
-    def scoreSeq(self, sequence):
-        """
-        Given a sequence, find the score of that given sequence and return that score
-        """
-        score_temp = 0
-        i = 0
-        for nuc in str(sequence):
-            # if the thing is an N, then the score is a 0
-            if 'N' in nuc:
-                score_temp += 0 
-            else:
-                print(i)
-                print(nuc)
-                score_temp += float(self.pwm[self.nucs[nuc]][i])
-            i += 1
-        return score_temp
     
     def printName(self):
         print(self.name)
@@ -296,5 +157,4 @@ class KnownMotif:
         
     # To String method    
     def __str__(self):
-        #self.setPval() # MOVE THIS LINE
         return "MOTIF: " + self.name + " threshold score: " + str(self.threshold_score) + " pval:" + str(self.pval)
--- mrsoftware/mrsoftware.py
+++ mrsoftware/mrsoftware.py
@@ -1,8 +1,8 @@
 #!/usr/bin/ python3
 #TODO EVERYTIME THERE ARE ACTUAL CHANGES, RERUN THE INSTALL COMMAND TO MAKE IT ACTUALLY WORK AS A UNIX COMMAND. ALSO CHANGE THE IMPORT TO "from . "
 import argparse
-from . import KnownMotif
-#import KnownMotif
+#from . import KnownMotif
+import KnownMotif
 import os
 from pyfaidx import Fasta
 import sys
@@ -111,13 +111,14 @@ def main():
     else:
         out_file = sys.stdout
 
+
     #~~~~~~~~~~~~~~~~~~~~~~~ BEGIN PIPELINE ~~~~~~~~~~~~~~~~~~~~~~~~#
     #get the sequences of all the peaks
     writeMessage("Getting peak sequences\n")
     peak_seq_list = getListOfPeakSeqFromBed(bed_path)
     writeMessage("There are " + str(len(peak_seq_list)) + " peaks. \n")
     writeMessage("Creating random sequences from the genome\n")
-    # hardcoding in chr17 and length of 17
+    # hardcoding in chr17 and length of 17 FIX THIS
     random_background_seqs_list = generateRandomSeqsFromChr("17", len(peak_seq_list), 75) #generateListOfRandomSeqsFromPeaks(getPeaksFromBed(bed_path))
     writeMessage("We are using " + str(len(random_background_seqs_list)) + " random sequences\n")
 
@@ -135,24 +136,14 @@ def main():
     writeMessage("Done creating " + str(amount) + " random sequenceis of length " + str(length_of_random) +"\n")
     for motif in known_motifs:
         if motif != None:
-            motif.magicDoAllFunction(peak_seq_list, random_generated_seqs_list) #MAKE THE PWM SOON
-    '''
-
-    writeMessage("There are " + str(len(known_motifs)) + " known motifs. \n", output_file=out_file)
-
-    # Score motifs based on peaks
-    #printMotfs(known_motifs)
-    scoreAllRandomSequencesForMotifs(random_seqs_list, known_motifs)
-    scoreAllSequencesForMotifs(peak_seq_list, known_motifs)
-    setAllThresholds(known_motifs, random_seqs_list, 0.001)
-    '''
+            motif.scoreAllGetThresholdScoreCalcPValue(peak_seq_list, random_generated_seqs_list) #MAKE THE PWM SOON
 
     known_motifs.sort(key = KnownMotif.KnownMotif.getPval)
     for motif in known_motifs:
         writeMessage(str(motif) + "\n", output_file=out_file)
     #returnMotifsWithLowestPval(known_motifs, 4)
-    out_file.close()
     sysMessage("DONE >:[ \n\n")
+    out_file.close()
 
 def generateRandomSeqsFromChr(chr, num, length):
     """
@@ -164,7 +155,6 @@ def generateRandomSeqsFromChr(chr, num, length):
         if toAdd != None:
             backgroundSeqs.append(toAdd)
     return backgroundSeqs
-
 
 def returnMotifsWithLowestPval(motif_array, num_of_motifs):
     """
@@ -195,284 +185,6 @@ def returnMotifsWithLowestPval(motif_array, num_of_motifs):
         i+=1
     return sorted_motifs[:num_of_motifs]
 
-
-def scoreAllSequencesForMotifs(peak_seq_list, motif_array):
-    """
-    Given a list of sequences and motifs, score all motifs with the sequences
-
-    Parameters:
-    -----------
-        peak_seq_list : List of sequences from peaks
-        motif_array : array of all the motifs to be scored
-
-    Returns:
-    --------
-        Nothing
-    """
-    for peak in peak_seq_list:
-        scoreAllMotifsByPeak(peak, motif_array)
-    
-def scoreAllRandomSequencesForMotifs(peak_seq_list, motif_array):
-    """
-    Given a list of sequences and motifs, score all motifs with the sequences
-
-    Parameters:
-    -----------
-        peak_seq_list : List of sequences from peaks
-        motif_array : array of all the motifs to be scored
-
-    Returns:
-    --------
-        Nothing
-    """
-    for peak in peak_seq_list:
-        scoreAllRandomMotifsByPeak(peak, motif_array)
-
-def scoreAllRandomMotifsByPeak(peak_seq, motif_array):
-    """
-    For a given sequence, score all motifs using that sequence. Updates all the motifs with new scores
-
-    Parameters:
-    -----------
-        peak_seq : Sequence of the peak to score motifs with
-        motif_array : array of all the known motif objects 
-
-    Returns:
-    --------
-        No Return
-    """
-    for motif in motif_array:
-        motif.scoreRandSequence(str(peak_seq))
-
-def scoreAllMotifsByPeak(peak_seq, motif_array):
-    """
-    For a given sequence, score all motifs using that sequence. Updates all the motifs with new scores
-
-    Parameters:
-    -----------
-        peak_seq : Sequence of the peak to score motifs with
-        motif_array : array of all the known motif objects 
-
-    Returns:
-    --------
-        No Return
-    """
-    for motif in motif_array:
-        motif.scoreSequence(peak_seq)
-
-def getSeqFromPeakList(peaks):
-    """
-    peaks - 
-    returns the sequences of the peaks
-    """
-    peaks_seq_list = []
-    for peak in peaks:
-        peaks_seq_list.append(getSeqFromPeak(peak))
-    return peaks_seq_list
-
-def getSeqFromPeak(peak):
-    """
-    returns
-    """
-    return getSeqFromGenome(peak[0],peak[1],peak[2])
-
-def GetPFM(sequences):
-    """ Compute the PFM for a set of sequences
-    
-    Parameters
-    ----------
-    sequences : list of str
-        List of sequences (e.g. binding_sites)
-    
-    Returns
-    -------
-        pfm : 2d np.array
-        
-    Assumes all sequences have the same length
-    """
-    pfm = np.zeros((4, len(sequences[0])))
-    # pfm[i,j]: value for position j at nucleotide i
-    for seq in sequences:
-        i = 0
-        while i < len(seq):
-            if seq[i] == 'A':
-                pfm[0,i] = pfm[0,i] + 1
-            elif seq[i] == 'C':
-                pfm[1,i] = pfm[1,i] + 1
-            elif seq[i] == 'G':
-                pfm[2,i] = pfm[2,i] + 1
-            elif seq[i] == 'T':
-                pfm[3,i] = pfm[3,i] + 1
-            i += 1
-            
-    return pfm
-
-def GetBackgroundFreqs(meme_path):
-    """ Get background freqs from meme file
-    
-    Parameters
-    ----------
-        meme_path = meme file that was inputed
-
-    Returns
-    -------
-        list of background freqs in order of A, C, G, T
-    """
-
-    background_freqs = []
-    file = open(meme_path)
-    prev_line = ''
-    back_seq_line = ''
-    for line in file.readlines():
-        if 'Background letter frequencies' in line: 
-            prev_line = line
-        elif prev_line != '':
-            back_seq_line = line
-            break
-    split_text = back_seq_line.split(" ")
-    background_freqs.append(float(split_text[1].strip()))
-    background_freqs.append(float(split_text[3].strip()))
-    background_freqs.append(float(split_text[5].strip()))
-    background_freqs.append(float(split_text[7].strip()))
-    return background_freqs
-
-def GetPWM(binding_sites, background_freqs=[0.25, 0.25, 0.25, 0.25]):
-    """ Compute the PWM for a set of binding sites
-    
-    Parameters
-    ----------
-    binding_sites : list of str
-        List of sequences 
-    background_freqs: list of float
-        Background frequency of A, C, G, T
-    
-    Returns
-    -------
-        pwm : 2d np.array
-        
-    Assumes all sequences have the same length
-    """
-    pwm = np.zeros((4, len(binding_sites[0])))
-    pfm = GetPFM(binding_sites)
-    pfm = pfm + 0.01 # Add pseudocount. Don't change this!
-    # Compute pwm below
-    # Note: np.sum(pfm[:,j]) will give the sum of counts for column j
-    # Note: pfm[i,j]/np.sum(pfm[:,j]) gives p(i,j) (frequency of nucleotide i at position j)
-    j = 0
-    while j < len(binding_sites[0]):
-        i = 0
-        while i < 4:
-            pwm[i,j] = math.log2((pfm[i,j]/np.sum(pfm[:,j]))/background_freqs[i])
-            i+=1
-        j+=1        
-    return pwm
-
-def ScoreSeq(pwm, sequence):
-    """ Score a sequence using a PWM
-    
-    Parameters
-    ----------
-    pwm : 2d np.array
-       Position weight matrix
-    sequence : str
-       Sequence of nucleotides to be scored
-       
-    Returns
-    -------
-    score : float
-       PWM score of the sequence
-    """
-    score = 0
-    i = 0
-    while i < len(sequence):
-        if sequence[i] == 'A':
-            score += pwm[0, i]
-        elif sequence[i] == 'C':
-            score += pwm[1, i]
-        elif sequence[i] == 'G':
-            score += pwm[2, i]
-        elif sequence[i] == 'T':
-            score += pwm[3, i]
-        i = i+1
-    return score
-
-def GetThreshold(null_dist, pval):
-    """ Find the threshold to achieve a desired p-value
-    
-    Given a null distribution (list of values),
-    find the threshold to achieve a desired p-value
-    
-    Parameters
-    ----------
-    null_dist : list of float
-       Null distribution of scores (random scores)
-    pval : float
-       % of null_dist that should be above the threshold returned
-       
-    Returns
-    -------
-    thresh : float
-       Threshold to achieve the desired p-value    
-    """
-    thresh = 0 # set this below to be the score threshold to obtain a p-value <0.01
-    num = len(null_dist) * pval
-    temp_dist = null_dist.copy()
-    i = 1
-    while i < num:
-        temp_dist.pop(temp_dist.index(max(temp_dist)))
-        i+=1
-    thresh = max(temp_dist)
-    return thresh
-
-def ScanSequence(pwm, sequence):
-    """ Scan a sequence using a PWM
-    
-    Parameters
-    ----------
-    pwm : 2d np.array
-        PWM matrix
-    sequence : str
-        Long nucleotide string
-        
-    Returns
-    -------
-    scores : list of float
-        scores[i] should give the score of the substring sequence[i:i+n]
-    """
-    n = pwm.shape[1]
-    scores = [0]*(len(sequence)-n+1) # list of scores. scores[i] should give the score of the substring sequence[i:i+n]
-    i = 0
-    while i < (len(sequence)-n+1):
-        scores[i] = ScoreSeq(pwm, sequence[i:i+n])
-        i+=1
-    return scores
-
-def ReverseComplement(sequence):
-    """ Get the reverse complement of a sequence
-    
-    Parameters
-    ----------
-    sequence : str
-      Sequence of nucleotides
-      
-    Returns
-    -------
-    revcomp : str
-      Reverse complement of sequence
-    """
-    revcomp = ""
-    i = len(sequence)-1
-    while i >= 0:
-        if sequence[i] == 'A':
-            revcomp = revcomp + 'T'
-        elif sequence[i] == 'C':
-            revcomp = revcomp + 'G'
-        elif sequence[i] == 'G':
-            revcomp = revcomp + 'C'
-        elif sequence[i] == 'T':
-            revcomp = revcomp + 'A'
-        i = i-1
-    return revcomp
 def randomSeqFromFrequencies(frequencies, length):
     """
     Given the frequencies and desiered length, generate a random sequence to fit the request.
@@ -483,54 +195,11 @@ def randomSeqFromFrequencies(frequencies, length):
         sequence += random.choices(nuceleotides,weights=frequencies)[0]
     return sequence
 
-
-
 def createArrOfRandomlyGeneratedSeqs(num, frequencies,length):
     """
     Given a number of sequences to generate, the frequencies at which those appear, and the length of the sequence, return an array of sequences
     """
     return [randomSeqFromFrequencies(frequencies, length) for i in range(num)]
-
-
-
-def ComputeNucFreqs(sequences, background_freqs = [0.25,0.25,0.25,0.25]):
-    """ 
-    Compute nucleotide frequencies of a list of sequences
-    
-    Parameters
-    ----------
-    sequences : list of str
-       List of sequences
-       
-    Returns
-    -------
-    freqs : list of float
-       Frequencies of A, C, G, T in the sequences
-    """
-    freqs = [0.25, 0.25, 0.25, 0.25] # compute frequency of A, C, G, T
-    total = 0
-    A_count = 0
-    C_count = 0
-    G_count = 0
-    T_count = 0
-    for seq in sequences:
-        total += len(seq)
-        i = 0
-        while i < len(seq):
-            if seq[i] == 'A':
-                A_count = A_count + 1
-            elif seq[i] == 'C':
-                C_count = C_count + 1
-            elif seq[i] == 'G':
-                G_count = G_count + 1
-            elif seq[i] == 'T':
-                T_count = T_count + 1
-            i+=1
-    freqs[0] = A_count/total
-    freqs[1] = C_count/total
-    freqs[2] = G_count/total
-    freqs[3] = T_count/total
-    return freqs
 
 def getSeqFromGenome(chromosome, start, end):
     """
@@ -545,6 +214,7 @@ def getSeqFromGenome(chromosome, start, end):
     Returns: 
     string of nucleotides
     """
+    
     #genes = Fasta(genome)
     chromosome = str(chromosome).removeprefix("chr")
     if chromosome in genome: # if chromosome is in the same format as the indexing
@@ -553,7 +223,6 @@ def getSeqFromGenome(chromosome, start, end):
         return str(genome["chr"+str(chromosome)][start:end])
     else:
         print("CHROMOSOME NOT FOUND!!!!! in getSeqFromGenome >:[ Params were: " + str(chromosome) +" at "+ str(start) +":"+ str(end)) # TODO CHANGE THIS to a REAL ERROR Rather than this print
-
 
 def getPeaksFromBed(bed_path):
     """
@@ -564,11 +233,11 @@ def getPeaksFromBed(bed_path):
 
     Parameters:
     -----------
-        meme_path : path to the meme file
+        bed_path : path to the bed file
 
     Returns:
     --------
-        known_motifs_list : list of known motifs
+        peaks: list of locations of peaks in the format {chr, start, stop}
     
     """
     peaks = []
@@ -583,23 +252,20 @@ def getPeaksFromBed(bed_path):
 def getListOfPeakSeqFromBed(bed_path):
     """
     Returns a list of all the peak sequences
-    Calls get peaks from Bed
-
-    Description
+    Calls getPeaksFromBed
 
     Parameters:
     -----------
-        meme_path : path to the meme file
+        bed_path : path to the bed file
 
     Returns:
     --------
-        known_motifs_list : list of known motifs
+        peak_list : list of peaks (nucleotide sequence)
     """
-    peaks = []
     peak_list = []
     ent_list = getPeaksFromBed(bed_path)
     for ent in ent_list:
-        temp_seq = getSeqFromGenome(int(ent[0]), int(ent[1]), int(ent[2]))
+        temp_seq = getSeqFromGenome(ent[0], int(ent[1]), int(ent[2]))
         if temp_seq != None: # will be None if the chromosome isnt available
             peak_list.append(str(temp_seq)) # add the string of the sequence
     return peak_list
@@ -675,12 +341,6 @@ def calculateBackgroundFrequencies(background_seqs):
         freqs[i] = counts[i]/total
     return freqs
 
-def getListOfReversePeakSeq(forward_seqs):
-    reverse_seqs = []
-    for seq in forward_seqs:
-        reverse_seqs.append(ReverseComplement(seq))
-    return reverse_seqs
-
 def getRandomBackgroundSeq(chr, length):
     """
     Given a chromosome, get a random sequence of that length
@@ -707,7 +367,19 @@ def getRandomBackgroundSeq(chr, length):
     return getSeqFromGenome(chr, start, start + length)
 
 def generateListOfRandomSeqsFromPeaks(peaks_info):
-    #print("running generateListOfRandomSeqsFromPeaks")
+    """
+    Creates a list of random sequences from function getPeaksFromBed
+
+    Parameters:
+    -----------
+    peaks_info: 
+        list of known motifs
+
+    Returns:
+    --------
+
+    """
+    
     #peaks_info from getPeaksFromBed 
     random_seqs = []
     for ent in peaks_info:
@@ -715,15 +387,6 @@ def generateListOfRandomSeqsFromPeaks(peaks_info):
         chr = ent[0]
         random_seqs.append(getRandomBackgroundSeq(chr,length))
     return random_seqs
-
-def setAllThresholds(known_motifs, background_seqs, pval):
-    for motif in known_motifs:
-        motif.calcAndSetThreshold(background_seqs, pval)
-
-def printMotfs(knownMotifs):
-    for motif in knownMotifs:
-        motif.printName()
-        print(motif.printPWM())
 
 # Run Main
 if __name__ == "__main__":
